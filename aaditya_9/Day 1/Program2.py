e96ef19428760d1cec83296b0506b485ab556d94
aaditya6106 <aaditya6106@gmail.com>
2025-08-28 12:11:29 +0530

    Create Program2.py

---
+++ aaditya_9/Day 1/Program2.py
@@ -0,0 +1,8 @@
+#program to store student name, age, marks in variable and print them
+
+name="Aadi"
+age=20
+marks=85
+print("\nStudent Name:",name)
+print("Student Age:",age)
+print("Student Marks:",marks,"\n")
