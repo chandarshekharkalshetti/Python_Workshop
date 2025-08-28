2b9d40591596578f31ea88c437c480249a3b0615
gundelidharani-afk <gundelidharani@gmail.com>
2025-08-28 11:55:22 +0530

    Update One.py

--- Dharani_00/Day_01/One.py
+++ Dharani_00/Day_01/One.py
@@ -1,1 +1,10 @@
-
+Print("hello world")
+Print name & age
+Print a multiple line string
+Print a pattern of stars
+Print the numbers from 1-10 using for Loop 
+Print even numbers between 1-20 
+Print off numbers between 1-20 
+Print a table of 5 
+Print ASCII value of a character 
+Print your college name in uppercase and lowercase 
